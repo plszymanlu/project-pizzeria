06b0c44eb589a0ac37c1fbf0071b10768994b688
lukasz <plszymanlu@gmail.com>
2020-10-04 20:03:37 +0200

    processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -78,6 +78,16 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
 
@@ -87,7 +97,7 @@
       thisProduct.accordionTrigger.addEventListener('click', function(){
       console.log('clicked');
       /* prevent default action for event */
-      Event.preventDefault();
+      event.preventDefault();
       /* toggle active class on element of thisProduct */
       thisProduct.element.classList.toggle('active');
       /* find all active products */
@@ -103,7 +113,71 @@
       /* END LOOP: for each active product */
     };
     /* END: click event listener to trigger */
-    },
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+      console.log(this.initOrderFrom);
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+        thisProduct.addToCart();
+      });
+    }
+
+  processOrder(){
+    const thisProduct = this;
+/* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+const formData = utils.serializeFormToObject(thisProduct.form);
+console.log('formDaya', formData);
+
+thisProduct.params = {};
+
+  /* set variable price to equal thisProduct.data.price */
+  let price = thisProduct.data.price;
+  console.log(price);
+  /* START LOOP: for each paramId in thisProduct.data.params */
+  for(let paramId in thisProduct.data.params) {
+  console.log('param:',param);
+    /* save the element in thisProduct.data.params with key paramId as const param */
+  const param = thisProduct.data.params[paramId];
+    /* START LOOP: for each optionId in param.options */
+    for(let optionId in param.options){
+      /* save the element in param.options with key optionId as const option */
+    const option = param.options[optionId];
+    const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+      /* START IF: if option is selected and option is not default */
+    if(optionSelected && !option.default){
+        /* add price of option to variable price */
+        price += option.price;
+      }
+      /* END IF: if option is selected and option is not default */
+      /* START ELSE IF: if option is not selected and option is default */
+        /* deduct price of option from price */
+        price -= option.price;
+      /* END ELSE IF: if option is not selected and option is default */
+    }
+    /* END LOOP: for each optionId in param.options */
+  }
+  /* END LOOP: for each paramId in thisProduct.data.params */
+  }
+  /* set the contents of thisProduct.priceElem to be the value of variable price */
+  thisProduct.priceElem.innerHTML = thisProduct.price;
+      console.log(thisProduct.params);
+}
+  }
 
     const app = {
     initMenu: function(){
@@ -132,6 +206,6 @@
       thisApp.initData();
       thisApp.initMenu();
     },
-  };
+  },
   app.init();
 }
